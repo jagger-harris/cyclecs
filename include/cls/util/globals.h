92a22e535878bffc9c7a2046bf9b3542d5e98e17
jagger-harris <jagger.lee.harris@gmail.com>
2026-07-13 18:52:52 -0500

    Removed unused string id max variable

--- include/cls/util/globals.h
+++ include/cls/util/globals.h
@@ -7,8 +7,6 @@
 enum {
     CLS_GLOBALS_PATH_MAX = 512, /**< Maximum length in bytes of a
                                    path. */
-    CLS_GLOBALS_STR_ID_MAX = 64, /**< Maximum length in bytes of a string
-                                    id before hashing. */
 };
 
 #endif // CLS_GLOBALS_H
